869eb3ddf39f52b6b760be06a300e1f715ee8cb5
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-05-23 11:07:39 -0300

    Define new create and show functions. Accepts arrays for votings and keywords.

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -8,6 +8,29 @@ module Api
 				render json: {status: 'SUCCESS', message:'Perguntas carregadas', data:questions}, status: :ok
 			end
 
+      # Listar pergunta passando o id
+      def show
+        question = Question.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Pergunta carregada', data: question}, status: :ok
+      end
+
+      # Criar nova pergunta passando os parâmetros
+      def create
+        question = Question.new(question_params)
+        if question.save
+          render json: {status: 'SUCCESS', message:'Pergunta salva', data:question},status: :ok
+        else
+          render json: {status: 'ERROR', message:'Pergunta não salva', data:question.erros},status: :unprocessable_entity
+        end
+      end
+
+      # Parâmetros da pergunta: Titulo, descrição, categoria,
+      # palavras-chave (array) e votações relacionadas (array) (não implementado ainda)
+      private
+      def question_params
+        params.permit(:title, :description, :category, :keywords => [], :votings => [])
+      end
+
 		end
 	end
 end
