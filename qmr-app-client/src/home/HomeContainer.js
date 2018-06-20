de8fe36b5f59cdf555884b5524805814a23af470
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-20 20:28:26 -0300

    Create HomeContainer

---
+++ qmr-app-client/src/home/HomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+    render(){
+        return(
+            <div>
+                Componente responsável pelas perguntas que o usuário vai responder...
+                Também deverá mostrar os cálculos e os deputados que mais se parecem com ele.
+            </div>
+        );
+    }
+}
+
+export default HomeContainer;
